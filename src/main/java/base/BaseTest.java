6ffb01ff171666b472f4ba52b803e5a5d722f3da
anudeepm215 <anudeepm215@gmail.com>
2026-01-21 15:38:29 +0530

    Add BaseTest for test setup and teardown

---
+++ src/main/java/base/BaseTest.java
@@ -0,0 +1,24 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utils.DriverFactory;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = DriverFactory.initDriver();
+        driver.get("https://example-hr-app.com");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
